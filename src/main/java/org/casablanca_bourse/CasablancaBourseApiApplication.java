a45572816c2d66f8345d2723b3c7008a8402e28b
Amine DAOUDI <daoudicpge@gmail.com>
2016-12-17 09:04:39 -0800

    Creation of the project

---
+++ src/main/java/org/casablanca_bourse/CasablancaBourseApiApplication.java
@@ -0,0 +1,12 @@
+package org.casablanca_bourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CasablancaBourseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CasablancaBourseApiApplication.class, args);
+	}
+}
---
+++ src/test/java/org/casablanca_bourse/CasablancaBourseApiApplicationTests.java
@@ -0,0 +1,16 @@
+package org.casablanca_bourse;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CasablancaBourseApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
